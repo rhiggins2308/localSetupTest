622189a41c23dc6f1067a771354967d331ddd258
rhiggins2308 <rhiggins2308@gmail.com>
2020-10-01 10:50:18 +0100

    first commit

---
+++ FirstBitBucket/src/com/ait/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.ait.git;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("Git Test. Added locally");
+		System.out.println("Test for pull. Added in Bitbucket");
+		
+		System.out.println("Lab 3 - Edit locally");
+		System.out.println("Lab 3 - Edit remotely");
+		
+	}
+
+}
